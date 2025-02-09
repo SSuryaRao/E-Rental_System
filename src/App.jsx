36a4493191d52f548a3483e7032742a36e7fdc0b
SSuryaRao <suryaraosodi@gmail.com>
2025-02-09 20:00:07 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline text-center text-yellow-300 bg-amber-600">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
