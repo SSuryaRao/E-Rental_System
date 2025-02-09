ee51190ada2ef7082c6cddc1f3e3bb348364e90e
SSuryaRao <suryaraosodi@gmail.com>
2025-02-09 20:16:02 +0530

    adding pages

---
+++ src/pages/loginpage/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function LoginPage() {
+    return ( 
+        <div>
+            <h1>Welcome to the LoginPage    </h1>
+        </div>
+     );
+}
+
+export default LoginPage;
