d337cfe2d536a97bd92024b0c6f79437d64e88b4
SSuryaRao <suryaraosodi@gmail.com>
2025-03-21 20:39:29 +0530

    changing product page

--- src/pages/productspage/ProductPage.jsx
+++ src/pages/productspage/ProductPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero';
 import Categories from './categories';
+import Products from './Products';
 
 function ProductPage() {
     return ( 
         <div>
             <Hero />
             <Categories />
+            <Products />
         </div>
      );
 }
